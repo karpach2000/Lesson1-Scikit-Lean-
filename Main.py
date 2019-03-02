616090f2277128bda51a69e0b728228784f7a32b
karpach2000 <karpach2000@yandex.ru>
2019-03-02 09:28:54 +0300

    Добавленна кластеризация

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt 
 import numpy as np 
 import seaborn as sns; sns.set()
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
 
 
 print("Каждая строка в данной таблице соответсвует одному цветку\n")
@@ -44,3 +49,27 @@ plt.plot(xfit, yfit)#обученная модель
 plt.show()
 
 print('Пример обучения с учителем')
+Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 1)#обучающая последовательность и контрольная последовательность
+model = GaussianNB()#создаем экземпляр модели
+model.fit(Xtrain, ytrain)#обучаем модель  на данных
+y_model = model.predict(Xtest)#предсказываем значения для новых данных
+print('Процент предсказанных меток, соответсвующих истиному значению: ', accuracy_score(ytest, y_model))
+
+print('Пример обучения без учителя')
+model = PCA(n_components=2)
+model.fit(X_iris)
+X_2D = model.transform(X_iris)
+iris['PCA1'] = X_2D[:,0]
+iris['PCA2'] = X_2D[:,1]
+sns.lmplot("PCA1", "PCA2", hue = 'species', data = iris, fit_reg = False)
+plt.title('Проекция данных набора Iris на двумерное пространство.')
+plt.show()
+
+print('Обучение без учителя: кластеризация наборов данных Iris')
+model = GaussianMixture(n_components = 3, covariance_type = 'full')#создаем экземпляр модели с гиперпараметрами
+model.fit(X_iris)
+y_gmm = model.predict(X_iris)
+iris['cluster'] = y_gmm
+sns.lmplot("PCA1", "PCA2", data = iris, hue = 'species', col = 'cluster', fit_reg = False)
+plt.title('Проекция данных набора Iris на двумерное пространство.')
+plt.show("Кластеризация методом k - средних в наборе данных Iris")
